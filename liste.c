2260a1429565be280f67b4d9d5fa45997f3ec829
nxp3 <moppapo@gmail.com>
2014-12-05 17:40:37 +0100

    Correction de la fonction ajouter_position

--- liste.c
+++ liste.c
@@ -3,59 +3,59 @@
 #include "liste.h"
 
 ListePosition *creer_liste_positions() {
-	/* Créé une nouvelle liste de positions.
-	   Retourne un pointeur vers celle-ci ou NULL en cas d'échec. */
-	
+	/* Crï¿½ï¿½ une nouvelle liste de positions.
+	   Retourne un pointeur vers celle-ci ou NULL en cas d'ï¿½chec. */
+
 	ListePosition *liste_positions = (ListePosition *)malloc(sizeof(ListePosition));
-	
+
 	if (liste_positions != NULL) {
 		liste_positions->nb_elements = 0;
 		liste_positions->debut       = NULL;
 		return liste_positions;
 	}
-	
+
 	return NULL;
 }
 
 int ajouter_position(ListePosition *listeP, int ligne, int ordre, int numero_phrase) {
-	/* Ajoute une position en respectant l'ordre du mot dans le texte à une ListePosition.
-	   Retourne 1 en cas de succès, sinon 0. */
-	
-	// Création d'une nouvelle position
+	/* Ajoute une position en respectant l'ordre du mot dans le texte ï¿½ une ListePosition.
+	   Retourne 1 en cas de succï¿½s, sinon 0. */
+
+	// Crï¿½ation d'une nouvelle position
 	Position *nouvelle_position = (Position *)malloc(sizeof(Position));
-	
+
 	if (nouvelle_position == NULL) return 0; // Une erreur d'allocation est survenue.
-	
+
 	nouvelle_position->numero_ligne  = ligne;
 	nouvelle_position->ordre         = ordre;
 	nouvelle_position->numero_phrase = numero_phrase;
 	nouvelle_position->suivant       = NULL;
-	
+
 	if (listeP->debut == NULL) { // Cas 1 : actuellement aucune position dans la liste.
 		listeP->debut = nouvelle_position;
 		listeP->nb_elements++;
 		printf("Ok 1\n"); // A supprimer plus tard
 		return 1;
-	} else { // Cas 2 : tous les autres cas. On parcourt la liste chaînée pour trouver la position d'insertion.
-		Position *positionTemp = listeP->debut; // Itérateur
-		
+	} else { // Cas 2 : tous les autres cas. On parcourt la liste chaï¿½nï¿½e pour trouver la position d'insertion.
+		Position *positionTemp = listeP->debut; // Itï¿½rateur
+
 		while(positionTemp->suivant != NULL) { // Tant que nous ne sommes pas au bout de la ListePosition
-			// INCOHERENCE AU NIVEAU DES CONDITIONS, A REVOIR !!!
-			if (positionTemp->suivant == NULL) { // Ajout de la position en queue de liste
-				positionTemp->suivant = nouvelle_position;
-				nouvelle_position->suivant = NULL;
-				listeP->nb_elements++;
-				return 1;
-			} else if (positionTemp->numero_ligne <= nouvelle_position->numero_ligne && positionTemp->ordre <= nouvelle_position->ordre) { // L'élement suivant a été trouvé, on insère la nouvelle position juste avant celui-ci.
+			 if (positionTemp->numero_ligne <= nouvelle_position->numero_ligne && positionTemp->ordre <= nouvelle_position->ordre) { // L'ï¿½lement suivant a ï¿½tï¿½ trouvï¿½, on insï¿½re la nouvelle position juste avant celui-ci.
 				nouvelle_position->suivant = positionTemp->suivant;
 				positionTemp->suivant = nouvelle_position;
 				listeP->nb_elements++;
 				return 1;
 			}
-			
+
 			positionTemp = positionTemp->suivant;
 		}
-		
+
+		if (positionTemp->suivant == NULL) { // Ajout de la position en queue de liste
+			positionTemp->suivant = nouvelle_position;
+			listeP->nb_elements++;
+			return 1;
+		}
+
 		return 0;
 	}
 }
@@ -66,8 +66,8 @@ int main()
 	int resAjoutPosition = ajouter_position(nouvelleListePositions, 3, 10, 2);
 	int resAjoutPosition2 = ajouter_position(nouvelleListePositions, 4, 5, 2);
 	int resAjoutPosition3 = ajouter_position(nouvelleListePositions, 1, 26, 12);
-	
+
 	printf("NB elts : %d, Ajout reussi ? : %d, Ajout 2 reussi ? : %d, Ajout 3 reussi ? : %d\n", nouvelleListePositions->nb_elements, resAjoutPosition, resAjoutPosition2, resAjoutPosition3);
-	
+
 	return 0;
 }
