512fa173e268084524ce9c48f2d4da54311c75e1
nxp3 <moppapo@gmail.com>
2014-12-06 15:57:41 +0100

    Ajout d'un début de la fonction ajouter_noeud

--- arbre.c
+++ arbre.c
@@ -4,26 +4,78 @@
 #include "arbre.h"
 
 ArbreBR *creer_abr() {
-	/* Cr�� un nouvel ABR vide.
-       Retourne un pointeur vers celui-ci ou NULL en cas d'�chec. */
-	
+	/* Cr�� un nouvel ABR vide.
+       Retourne un pointeur vers celui-ci ou NULL en cas d'�chec. */
+
 	ArbreBR *abr = (ArbreBR *)malloc(sizeof(ArbreBR));
-	
+
 	if (abr != NULL) {
 		abr->nb_mots_differents = 0;
 		abr->nb_mots_total      = 0;
 		abr->racine             = NULL;
 		return abr;
 	}
-	
+
 	return NULL;
+}
+
+int ajouter_noeud(ArbreBR *arbre, char *mot, int ligne, int ordre){
+	if (arbre == NULL || mot == NULL)
+		return 0; // Cas où l'arbre ou le mot ne sont pas définis
+
+
+	// AJOUT DU NŒUD DANS LA MEMOIRE
+	NoeudABR* noeudAAdd=(NoeudABR*)malloc(sizeof(NoeudABR));
+		// Ce nœud est le nœud qu'on veut ajouter.
+	if (noeudAAdd==NULL) return 0;
+		// On crée le nœud qu'on veut ajouter.
+	noeudAAdd->mot = mot;
+	noeudAAdd->filsGauche=NULL;
+	noeudAAdd->filsDroit=NULL;
+
+	// On crée la liste des positions.
+
+	ListePosition* listePosN=creer_liste_positions();
+	if (ajouter_position(listePosN,ligne,ordre) == 0) return 0;
+	noeudAAdd->positions=*listePosN;
+
+	// AJOUT DU NŒUD DANS L'ARBRE DE RECHERCHE
+
+		// PREMIER CAS : IL N'Y A PAS DE NOEUD DANS L'ARBRE
+
+	if (arbre->racine == NULL){
+
+		abr->nb_mots_differents++;
+		abr->nb_mots_total++;
+		arbre->racine = noeudAAdd;
+		return 1;
+	}
+
+		// SECOND CAS : IL Y A DES NOEUDS DANS L'ARBRE
+
+	struct NoeudABR *iter=arbre->racine;
+	do{
+		if (strcmp(iter->mot,mot) == 0){
+			// alors il y a déjà une occurence du mot dans l'arbre
+			ajouter_position(iter->positions,ligne,ordre)
+		} else if (strcmp(iter->mot,mot) < 0){
+			if (iter->filsGauche != NULL) iter=iter->filsGauche;
+			else{
+				abr->nb_mots_differents++;
+
+				abr->nb_mots_total++;
+			}
+		} else if (strcmp(iter->mot,mot) > 0){
+
+		} else return 0;
+	}while(iter!=NULL);
 }
 /*
 int main()
 {
 	ArbreBR *nouvel = creer_abr();
-	
+
 	printf("NB elts : %d\n", nouvel->nb_mots_differents);
-	
+
 	return 0;
 }*/
--- outils.c
+++ outils.c
@@ -4,30 +4,30 @@
 int charger_fichier(ArbreBR *arbre, char *filename) {
 	FILE *fichier = fopen(filename, "r");
 	
-	if(fichier != NULL) { // Fichier ouvert avec succès.
+	if(fichier != NULL) { // Fichier ouvert avec succï¿½s.
 		char mot[100]; // Longueur maximale d'un mot.
 		char c;
 		int nombre_mots = 0, numero_ligne = 0, numero_mot = 0;
-		
+
 		do {
-			c = fscanf(fichier, "%s", mot); // Récupère un mot.
+			c = fscanf(fichier, "%s", mot); // Rï¿½cupï¿½re un mot.
 			printf("Mot : %s\t\t\t\t\tNumero : %d\tLigne : %d\tPosition : %d\n", mot, nombre_mots, numero_ligne, numero_mot); // Affiche ce mot.
-		    
+
 			if (c == '\n')
 		        numero_ligne++;
-			
+
 			nombre_mots++;
-		} while (c != EOF); // Arrête la lecture à la fin du fichier.
-		
+		} while (c != EOF); // Arrï¿½te la lecture ï¿½ la fin du fichier.
+
 		printf("Ok\n");
-		
+
 		fclose(fichier); // Fermeture du flux vers le fichier.
-		
+
 		return nombre_mots;
 	} else { // Erreur d'ouverture du fichier.
 		printf("Erreur de lecture du fichier %s.\n", filename);
 		fclose(fichier); // Fermeture du flux vers le fichier.
-		
+
 		return 0;
 	}
 }
@@ -36,8 +36,8 @@ int main()
 {
 	ArbreBR *nouvel = creer_abr();
 	int reschargement = charger_fichier(nouvel, "roman.txt");
-	
+
 	printf("NB elts : %d, Resultat : %d\n", nouvel->nb_mots_differents, reschargement);
-	
+
 	return 0;
 }
